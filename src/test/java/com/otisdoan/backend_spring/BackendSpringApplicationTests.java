32aaeeda69fd720b349d275afa17dbec27e7a436
otisdoan <ledoanhieu12a6@gmail.com>
2025-08-31 17:02:18 +0700

    demo java backend

---
+++ src/test/java/com/otisdoan/backend_spring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otisdoan.backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
